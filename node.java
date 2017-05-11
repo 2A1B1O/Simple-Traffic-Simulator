e768060168411be95fd16e938042563c3f6d9c9f
Aslıhan Hayta <aslihanhayta_12@hotmail.com>
2017-05-11 11:16:34 +0300

    node
    Node Class

---
+++ node.java
@@ -0,0 +1,17 @@
+
+public class node {
+	private int numberOfCars;
+	
+	public node(int ncars)
+	{
+		numberOfCars=ncars;
+	}
+public void setNumberOfCars(int newValue)
+{
+	numberOfCars=newValue;
+}
+public int getNumberOfCars()
+{
+	return numberOfCars;
+}
+}
