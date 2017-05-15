371053a99cddae1ae7accde43fe8d2c957e39ce8
Aslıhan Hayta <aslihanhayta_12@hotmail.com>
2017-05-16 00:38:11 +0300

    Add files via upload

--- node.java
+++ node.java
@@ -1,6 +1,8 @@
 
 public class node {
 	private int numberOfCars;
+	private double x;
+	private double y;
 	
 	public node(int ncars)
 	{
@@ -14,4 +16,20 @@ public int getNumberOfCars()
 {
 	return numberOfCars;
 }
+public void setX(double newX)
+{
+	x=newX;
+	}
+public double getX()
+{
+	return x;
+	}
+public void setY(double newY)
+{
+	y=newY;
+}
+public double getY()
+{
+	return y;
+	}
 }
