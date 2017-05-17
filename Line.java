6b41f1623e4df6005d20a298c8f9da049ecf4f95
aslihan-ozturk <aslihan-ozturk@hotmail.com>
2017-05-17 17:28:28 +0300

    Add files via upload
    created by Aslı

--- Line.java
+++ Line.java
@@ -1,21 +1,51 @@
+import java.io.Serializable;
 
-public class Line {
-	Dot startDot = new Dot();
-	Dot finishDot= new Dot();
-	
-    public	Line() {
-		
-	}
-    public void SetstartDot(Dot At) {
-    	startDot=At;
-    }
-    public Dot GetstartDot() {
-    	return startDot;
-    }
-    public void SetfinishDot(Dot At) {
-    	finishDot=At;
-    }
-    public Dot GetfinishDot() {
-    	return finishDot;
-    }
+public class Line implements Serializable {
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+private int x,y,x1,y1;
+
+public Line(int x, int y, int x1, int y1) {
+	super();
+	this.x = x;
+	this.y = y;
+	this.x1 = x1;
+	this.y1 = y1;
 }
+public Line(){}
+public int getX() {
+	return x;
+}
+
+public void setX(int x) {
+	this.x = x;
+}
+
+public int getY() {
+	return y;
+}
+
+public void setY(int y) {
+	this.y = y;
+}
+
+public int getX1() {
+	return x1;
+}
+
+public void setX1(int x1) {
+	this.x1 = x1;
+}
+
+public int getY1() {
+	return y1;
+}
+
+public void setY1(int y1) {
+	this.y1 = y1;
+}
+}
+
+
