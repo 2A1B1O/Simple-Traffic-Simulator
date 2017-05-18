e5a33cfed8da7330465c4c1f891dd2eaffa8a901
Aslıhan Hayta <aslihanhayta_12@hotmail.com>
2017-05-18 11:31:55 +0300

    Update node.java

--- node.java
+++ node.java
@@ -1,35 +1,20 @@
 
 public class node {
 	private int numberOfCars;
-	private double x;
-	private double y;
+	Dot nodeLocation = new Dot();
 	
 	public node(int ncars)
 	{
-		numberOfCars=ncars;
+		numberOfCars = ncars;
 	}
 public void setNumberOfCars(int newValue)
 {
-	numberOfCars=newValue;
+	numberOfCars = newValue;
 }
 public int getNumberOfCars()
 {
 	return numberOfCars;
 }
-public void setX(double newX)
-{
-	x=newX;
-	}
-public double getX()
-{
-	return x;
-	}
-public void setY(double newY)
-{
-	y=newY;
+
 }
-public double getY()
-{
-	return y;
-	}
-}
+
