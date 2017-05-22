afe9abfd5016d3eaa90b03d8f6a3b74fc1f1874e
aslihan-ozturk <aslihan-ozturk@hotmail.com>
2017-05-23 01:51:57 +0300

    Update Map.java

--- Map.java
+++ Map.java
@@ -3,39 +3,66 @@ import java.util.ArrayList;
 
 import org.w3c.dom.Node;
 
-public class Map implements Serializable{
-private ArrayList <node> MyNodes= new ArrayList();
-private ArrayList <Road> MyRoads= new ArrayList();
-private static final long serialVersionUID = 1L;
-public ArrayList<Road> getMyRoads() {
-	return MyRoads;
-}
-public void setMyRoads(ArrayList<Road> myRoads) {
-	MyRoads = myRoads;
-}
-private ArrayList <Roundabout> MyRoundabouts= new ArrayList();
+public class Map implements Serializable {
+	private ArrayList<node> MyNodes = new ArrayList();
+	public ArrayList<node> getMyNodes() {
+		return MyNodes;
+	}
 
-public Map(){
+	public void setMyNodes(ArrayList<node> myNodes) {
+		MyNodes = myNodes;
+	}
+
+	public ArrayList<Roundabout> getMyRoundabouts() {
+		return MyRoundabouts;
+	}
+
+	public void setMyRoundabouts(ArrayList<Roundabout> myRoundabouts) {
+		MyRoundabouts = myRoundabouts;
+	}
+
+	private ArrayList<Road> MyRoads = new ArrayList();
+	private ArrayList<Roundabout> MyRoundabouts = new ArrayList();
+	private static final long serialVersionUID = 1L;
+
+	public ArrayList<Road> getMyRoads() {
+		return MyRoads;
+	}
+
+	public void setMyRoads(ArrayList<Road> myRoads) {
+		MyRoads = myRoads;
+	}
+
 	
-}
-public void  AddRoad(Road r){
-	MyRoads.add(r);
-}
-public void AddRoundabout(Roundabout r){
-	MyRoundabouts.add(r);
-}
-public void  AddNode(node n){
-	MyNodes.add(n);
-}
-public void RemoveRoad(){
-	//arrayin sadece so elemaný deðil, istenilen elemaný silinmeli . arada boþluklar kalabilir mi ?
-	//öyleyse içine aldýðý elemaný arraylistte bulmak için önce search yapýp sonra silicek? diðerleriyle Tartýþ.
-}
-public void RemoveRoundabout(){
-	
-}
-public void RemoveNode(){
-	
+
+	public Map() {
+
+	}
+
+	public void AddRoad(Road r) {
+		MyRoads.add(r);
+	}
+
+	public void AddRoundabout(Roundabout r) {
+		MyRoundabouts.add(r);
+	}
+
+	public void AddNode(node n) {
+		MyNodes.add(n);
+	}
+
+	public void RemoveRoad() {
+		
+	}
+
+	public void RemoveRoundabout() {
+
+	}
+
+	public void RemoveNode() {
+
+	}
+
 }
 
 }
